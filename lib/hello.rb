a146de51ec1bab98eda8d7c38839fb2dd8a260b1
Antoni Romero Blanch <aromerob@uncc.edu>
2016-12-05 17:05:12 +0000

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
+require 'greeter'
+
 # Default is World
-# Author: Antoni Romero (aromerob@uncc.edu)
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
